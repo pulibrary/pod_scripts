d68e9f140bd7afd465c88611c0883ee54c2c8aca
Hector Correa <hector_correa@princeton.edu>
2021-03-15 16:41:09 -0400

    Uses STDOUT to save the file

--- pod_java.sh
+++ pod_java.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 CP=marc4j-2.9.1.jar
 ALMA_DIR="/Users/correah/data/fulldump_expanded"
-POD_DIR="./pod_files_java"
+POD_DIR="/Users/correah/data/pod_files_java"
 
 mkdir -p $POD_DIR
-rm "$POD_DIR/*"
+rm $POD_DIR/*.xml
 
 echo compiling...
 javac -cp $CP filter.java
@@ -15,7 +15,11 @@ fi
 
 for FILE in $ALMA_DIR/*.xml
 do
-  java -cp $CP:. filter $FILE
+  BASE_NAME=`basename $FILE`
+  POD_FILE="$POD_DIR/pod_$BASE_NAME"
+  echo "Processing $FILE"
+
+  java -cp $CP:. filter $FILE > $POD_FILE
   # For testing purposes just process one file
-  break
+  # break
 done
